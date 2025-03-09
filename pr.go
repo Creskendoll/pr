cef59eafbfacb7678ca9f542a22764bb64cfb1eb
Kenan Soylu <kenan.soylu@doctolib.com>
2025-03-09 22:14:19 +0100

    desc edit

--- pr.go
+++ pr.go
@@ -4,16 +4,13 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
 )
 
 func main() {
 	ctx := context.Background()
-
-	llmClient, err := LLMClient(ctx)
-	if err != nil {
-		fmt.Println("Error creating LLM client:", err)
-		os.Exit(1)
-	}
 
 	gitBranch, err := getBranch()
 	if err != nil {
@@ -28,13 +25,50 @@ func main() {
 		os.Exit(1)
 	}
 
+	llmClient, err := LLMClient(ctx)
+	if err != nil {
+		fmt.Println("Error creating LLM client:", err)
+		os.Exit(1)
+	}
+
 	description, err := diffDescription(llmClient, diff, ctx)
 	if err != nil {
 		fmt.Println("Error getting diff description:", err)
 		os.Exit(1)
 	}
 
-	prCreated, err := gh("pr", "create", "--title", fmt.Sprintf("\"%s\"", gitBranch), "--body", fmt.Sprintf("\"%s\"", description))
+	tmpFilePath := filepath.Join(os.TempDir(), fmt.Sprintf("pr-%s.md", gitBranch))
+	err = os.WriteFile(tmpFilePath, []byte(description), 0755)
+	if err != nil {
+		fmt.Println("Error writing description to file:", err)
+		os.Exit(1)
+	}
+
+	err = exec.Command("open", tmpFilePath).Run()
+	if err != nil {
+		fmt.Println("Error opening file:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Does the description look good? (y/n)")
+	var answer string
+	n, err := fmt.Scanln(&answer)
+	if err != nil {
+		fmt.Println("Error reading input:", err)
+		os.Exit(1)
+	}
+	if n != 1 || strings.ToLower(answer) != "y" || strings.ToLower(answer) != "yes" {
+		fmt.Println("Exiting")
+		os.Exit(0)
+	}
+
+	editedDescription, err := os.ReadFile(tmpFilePath)
+	if err != nil {
+		fmt.Println("Error reading edited description:", err)
+		os.Exit(1)
+	}
+
+	prCreated, err := gh("pr", "create", "--title", fmt.Sprintf("\"%s\"", gitBranch), "--body", fmt.Sprintf("\"%s\"", editedDescription))
 	if err != nil {
 		fmt.Println("Error creating PR:", err)
 		os.Exit(1)
